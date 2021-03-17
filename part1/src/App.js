c234c9d28abafa2e9c9fddde2bd141d4511f0e9a
Jetlum <jetlumajeti@gmail.com>
2021-03-17 19:29:12 +0100

    Clean Code: beautify code

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
-const Title 		=	props => <h1>{props.title}</h1>
-const Statistics 	=	props => <h1>{props.stats}</h1>
+const Title = props => <h1>{props.title}</h1>
+const Statistics = props => <h1>{props.stats}</h1>
 
 const Button = ({handleClick, text}) => {
 	return (
@@ -31,9 +31,9 @@ const App = () => {
 	// save clicks of each button to its own state
 	const title = 'give feedback'
 	const stats = 'statistics'
-	const [ good, setGood ]				=	useState(0)
-	const [ neutral, setNeutral ]		=	useState(0)
-	const [ bad, setBad ]				=	useState(0)
+	const [ good, setGood ] =	useState(0)
+	const [ neutral, setNeutral ] =	useState(0)
+	const [ bad, setBad ] =	useState(0)
 
 	const handleGoodClick = () => {
 		setGood(good + 1)
@@ -48,9 +48,9 @@ const App = () => {
 	return (
 		<div>
 			<Title title={title} />
-			<Button handleClick={handleGoodClick}		text='good' 	/>
-			<Button handleClick={handleNeutralClick}	text='neutral'	/>
-			<Button handleClick={handleBadClick} 		text='bad'		/>
+			<Button handleClick={handleGoodClick} text='good' />
+			<Button handleClick={handleNeutralClick} text='neutral' />
+			<Button handleClick={handleBadClick} text='bad' />
 			<Statistics stats={stats} />
 			<Good text='good' good={good} />
 			<Neutral text='neutral' neutral={neutral} />
