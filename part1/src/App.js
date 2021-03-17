1f5fd3dd04b500feefc0d5bc5d513f177e527bf1
Jetlum <jetlumajeti@gmail.com>
2021-03-17 20:40:43 +0100

    Update: added all, average and positive stats(temporary solutions)

--- part1/src/App.js
+++ part1/src/App.js
@@ -26,6 +26,21 @@ const Bad = ({text, bad}) => {
 		<p> {text} {bad} </p>
 	)
 }
+const All = ({text, good, neutral, bad}) => {
+	return (
+		<p> {text} {good + neutral + bad}</p>
+	)
+}
+const Average = ({text, good, neutral, bad, goodValue, neutralValue, badValue}) => {
+	return (
+		<p>{text} {(goodValue + neutralValue + badValue) / (good + neutral + bad)}</p>
+	)
+}
+const Positive = ({text, good, neutral, bad}) => {
+	return (
+		<p> {text} {(good / (good + neutral + bad)) * 100} %</p>
+	)
+}
 
 const App = () => {
 	// save clicks of each button to its own state
@@ -34,15 +49,21 @@ const App = () => {
 	const [ good, setGood ] = useState(0)
 	const [ neutral, setNeutral ] =	useState(0)
 	const [ bad, setBad ] =	useState(0)
+	const [goodValue, setgoodValue] = useState(0)
+	const [neutralValue, setneutralValue] = useState(0)
+	const [badValue, setbadValue] = useState(0)
 
 	const handleGoodClick = () => {
 		setGood(good + 1)
+		setgoodValue(goodValue + 1)
 	}
 	const handleNeutralClick = () => {
 		setNeutral(neutral + 1)
+		setneutralValue(neutralValue * 0)
 	}
 	const handleBadClick = () => {
 		setBad(bad + 1)
+		setbadValue((badValue * 0) - 1)
 	}
 
 	return (
@@ -55,6 +76,9 @@ const App = () => {
 			<Good text='good' good={good} />
 			<Neutral text='neutral' neutral={neutral} />
 			<Bad text='bad' bad={bad} />
+			<All text='all' good={good} neutral={neutral} bad={bad} />
+			<Average text='average' good={good} neutral={neutral} bad={bad} goodValue={goodValue} neutralValue={neutralValue} badValue={badValue} />
+			<Positive text='positive' good={good} neutral={neutral} bad={bad} />
 		</div>
 	)
 }
