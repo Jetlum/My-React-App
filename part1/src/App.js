84b14ffadb0279b806c7c1de2ed85e1a4fcdc214
Jetlum <jetlumajeti@gmail.com>
2021-03-18 14:36:46 +0100

    Clean Code: refactoring the components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,84 +1,81 @@
 import React, { useState } from 'react'
 
-const Title = ({title}) => <h1>{title}</h1>
-const Statistics = ({stats}) => <h1>{stats}</h1>
+const Header = ({text}) =>(
+		<>
+			<h1>{text}</h1>
+		</>
+	)
 
-const Button = ({handleClick, text}) => {
+const Button = ({handleClick, text}) => (
+	<button onClick={handleClick}>
+		{text}
+	</button>
+)
+
+const Statistics = ({ statistics }) => {
+	if(statistics.all.value === 0){
+		return (
+			<>
+				No feedback given
+			</>
+		)
+	}
 	return (
-		<button onClick={handleClick}>
-			{text}
-		</button>
-	)
+		<>
+  			<Statistic text={statistics.good.text} value={statistics.good.value} />
+  			<Statistic text={statistics.neutral.text} value={statistics.neutral.value} />
+  			<Statistic text={statistics.bad.text} value={statistics.bad.value} />
+  			<Statistic text={statistics.all.text} value={statistics.all.value} />
+  			<Statistic text={statistics.average.text} value={statistics.average.value} />
+  			<Statistic text={statistics.positive.text} value={statistics.positive.value} />
+  		</>
+  	)
 }
 
-const Good = ({text, good}) => {
+const Statistic = ({text, value}) => {
 	return (
-		<p> {text} {good} </p>
-	)
-}
-const Neutral = ({text, neutral}) => {
-	return (
-		<p> {text} {neutral} </p>
-	)
-}
-const Bad = ({text, bad}) => {
-	return (
-		<p> {text} {bad} </p>
-	)
-}
-const All = ({text, good, neutral, bad}) => {
-	return (
-		<p> {text} {good + neutral + bad}</p>
-	)
-}
-const Average = ({text, good, neutral, bad, goodValue, neutralValue, badValue}) => {
-	return (
-		<p>{text} {(goodValue + neutralValue + badValue) / (good + neutral + bad)}</p>
-	)
-}
-const Positive = ({text, good, neutral, bad}) => {
-	return (
-		<p> {text} {(good / (good + neutral + bad)) * 100} %</p>
+		<p> {text} {value} </p>
 	)
 }
 
 const App = () => {
 	// save clicks of each button to its own state
-	const title = 'give feedback'
-	const stats = 'statistics'
 	const [ good, setGood ] = useState(0)
 	const [ neutral, setNeutral ] =	useState(0)
 	const [ bad, setBad ] =	useState(0)
-	const [goodValue, setgoodValue] = useState(0)
-	const [neutralValue, setneutralValue] = useState(0)
-	const [badValue, setbadValue] = useState(0)
 
 	const handleGoodClick = () => {
 		setGood(good + 1)
-		setgoodValue(goodValue + 1)
 	}
 	const handleNeutralClick = () => {
 		setNeutral(neutral + 1)
-		setneutralValue(neutralValue * 0)
 	}
 	const handleBadClick = () => {
 		setBad(bad + 1)
-		setbadValue((badValue * 0) - 1)
 	}
+	const all = good + neutral + bad
+	const sum = good * 1 + bad * -1
+	const average = sum / all || 0
+	const positive = `${(good / all) * 100 || 0} %`
+
+	const statistics = {
+		good: {text: 'Good', value: good},
+		neutral: {text: 'Neutral', value: neutral},
+		bad: {text: 'Bad', value: bad},
+		all: {text: 'All', value: all},
+		average: {text: 'Average', value: average},
+		positive: {text: 'Positive', value: positive}
+	}
+
 
 	return (
 		<div>
-			<Title title={title} />
+			<Header text='Give Feedback' />
 			<Button handleClick={handleGoodClick} text='good' />
 			<Button handleClick={handleNeutralClick} text='neutral' />
 			<Button handleClick={handleBadClick} text='bad' />
-			<Statistics stats={stats} />
-			<Good text='good' good={good} />
-			<Neutral text='neutral' neutral={neutral} />
-			<Bad text='bad' bad={bad} />
-			<All text='all' good={good} neutral={neutral} bad={bad} />
-			<Average text='average' good={good} neutral={neutral} bad={bad} goodValue={goodValue} neutralValue={neutralValue} badValue={badValue} />
-			<Positive text='positive' good={good} neutral={neutral} bad={bad} />
+			<Header text='Statistics' />
+			<Statistics statistics={statistics} />
 		</div>
 	)
 }
