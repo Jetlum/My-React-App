c02cf9d9fac6b57b994b867408b7a347c101eae0
Jetlum <jetlumajeti@gmail.com>
2021-03-22 15:19:16 +0100

    Update: vote for the displayed anecdote

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -3,31 +3,44 @@ import React, { useState } from 'react'
 const Button = ({handleClick, text}) => {
 	return (
 		<>
-			<br/><button onClick={handleClick}> {text} </button>
+			<button onClick={handleClick}> {text} </button>
 		</>
 	)
 }
+const Anecdote = ({anecdote, votes}) => (
+	<>
+		<p>{anecdote}</p>
+		<p>{votes}</p>
+	</>
+)
+
 
 const App = () => {
 	const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+	    'If it hurts, do it more often',
+	    'Adding manpower to a late software project makes it later!',
+	    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	    'Premature optimization is the root of all evil.',
+	    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  	]
+  const initialVotes = Array(anecdotes.length).fill(0)
   const [ selected, setSelected ] = useState(0)
-  
-  const handleSelectedAnecdote = () => {
-  	const randomAnecodotesNumber = `${Math.floor(Math.random(selected) * (anecdotes.length - 1))}`
-  	setSelected(randomAnecodotesNumber)
+  const [ votes, setVote ] = useState(initialVotes)
+
+  const showAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length));
+
+  const voteAnecdote = () => {
+  	const copy = [...votes]
+  	copy[selected] += 1
+  	setVote(copy)
   }
-	
+
   return (
   	<div>
-  		{anecdotes[selected]}
-  		<Button handleClick={handleSelectedAnecdote} text='Next Anecdote' />
+  		<Anecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+  		<Button handleClick={voteAnecdote} text='Vote' />
+  		<Button handleClick={showAnecdote} text='Next Anecdote' />
   	</div>
   )
 
