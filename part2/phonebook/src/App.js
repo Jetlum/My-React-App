94a45a01b3f348f609581ef5b31faca7e4704b80
Jetlum <jetlumajeti@gmail.com>
2021-03-30 21:13:18 +0200

    Update: Added better solution to check and alert duplicate contact

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,51 +4,47 @@ import Contacts from './components/Contacts'
 const App = (props) => {
   	const [persons, setPersons] = useState(props.contacts) 
   	const [newName, setNewName] = useState('')
-
+  	
   	const addContact = (event) => {
   		event.preventDefault()
-  		const nameObject = {
+  		const person = {
   			name: newName,
   			date: new Date().toISOString(),
   			important: Math.random() < 0.5,
   			id: newName + '-' + (persons.length + 1)
   		}
-  		setPersons(persons.concat(nameObject))
-  		setNewName('')
+  		const contactExists = person => persons.some(p => p.name === person.name)
+
+  		if (contactExists(person)) {
+  			window.alert(`${person.name} is already added to phonebook`)
+  			setNewName('')
+  		} else {
+  			setPersons(persons.concat(person))
+  			setNewName('')
+  		}
 
   	}
-  	const handleNameChange = (event) => {
-  		setNewName(event.target.value)
-  	}
-
-  	function checkDuplicateContact(persons) {
-  		const personsLength = persons.length - 1 
-  		alert(`${newName} is already added to phonebook`)
-  		return persons.filter(person => persons[personsLength].name)
-	}
-
-  	const found = persons.find( ({ name }) => name === newName );
-  	const noDuplicateContact = found ? checkDuplicateContact(persons) : persons
+  	const handleNameChange = event => setNewName(event.target.value)
   	
-	  return (
-	    <div>
-	      <h2>Phonebook</h2>
-	      <form onSubmit={addContact}>
-	        <div>
-	          Write your name: <input value={newName} onChange={handleNameChange} />
-	        </div>
-	        <div>
-	          <button id="add-contact-button" type="submit">Add Contact</button>
-	        </div>
-	      </form>
-	      <h2>Contacts</h2>
-	      <ul>
-	        {noDuplicateContact.map(person => 
-	          <Contacts key={person.id} person={person} />
-	        )}
-	      </ul>
-	    </div>
-	  )
+	return (
+		<div>
+	    	<h2>Phonebook</h2>
+	      	<form onSubmit={addContact}>
+	        	<div>
+	          		Write your name: <input value={newName} onChange={handleNameChange} />
+	        	</div>
+	        	<div>
+	          		<button id="add-contact-button" type="submit">Add Contact</button>
+	        	</div>
+	      	</form>
+	      	<h2>Contacts</h2>
+	  		<ul>
+		        {persons.map(person => 
+		          <Contacts key={person.id} person={person} />
+		        )}
+	    	</ul>
+		</div>
+	)
 }
 
 export default App
