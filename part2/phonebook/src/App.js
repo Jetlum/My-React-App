06eecf5c00ba79cbd9632cb6c164b1de994351a0
Jetlum <jetlumajeti@gmail.com>
2021-03-30 20:57:46 +0200

    Update: Added function to handle duplicate message and return a filtered array without the duplicate name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -20,8 +20,15 @@ const App = (props) => {
   	const handleNameChange = (event) => {
   		setNewName(event.target.value)
   	}
+
+  	function checkDuplicateContact(persons) {
+  		const personsLength = persons.length - 1 
+  		alert(`${newName} is already added to phonebook`)
+  		return persons.filter(person => persons[personsLength].name)
+	}
+
   	const found = persons.find( ({ name }) => name === newName );
-  	const noDuplicateContact = found ? alert(`${newName} is already added to phonebook`) : persons
+  	const noDuplicateContact = found ? checkDuplicateContact(persons) : persons
   	
 	  return (
 	    <div>
@@ -31,12 +38,12 @@ const App = (props) => {
 	          Write your name: <input value={newName} onChange={handleNameChange} />
 	        </div>
 	        <div>
-	          <button type="submit">Add Contact</button>
+	          <button id="add-contact-button" type="submit">Add Contact</button>
 	        </div>
 	      </form>
 	      <h2>Contacts</h2>
 	      <ul>
-	        {persons.map(person => 
+	        {noDuplicateContact.map(person => 
 	          <Contacts key={person.id} person={person} />
 	        )}
 	      </ul>
