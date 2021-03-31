25e58b7beacc025287c2233c04162b3ce6983f0e
Jetlum <jetlumajeti@gmail.com>
2021-03-31 20:53:09 +0200

    Clean Code: Code refactoring

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,50 +1,48 @@
-import React, { useState } from 'react'
-import Contacts from './components/Contacts'
+	import React, { useState } from 'react'
+	import Contacts from './components/Contacts'
 
-const App = (props) => {
-  	const [persons, setPersons] = useState(props.contacts) 
-  	const [newName, setNewName] = useState('')
-  	
-  	const addContact = (event) => {
-  		event.preventDefault()
-  		const person = {
-  			name: newName,
-  			date: new Date().toISOString(),
-  			important: Math.random() < 0.5,
-  			id: newName + '-' + (persons.length + 1)
-  		}
-  		const contactExists = person => persons.some(p => p.name === person.name)
+	const App = ({ contacts }) => {
+	  	const [persons, setPersons] = useState(contacts) 
+	  	const [newName, setNewName] = useState('')
+	  	
+	  	const addContact = (event) => {
+	  		event.preventDefault()
+	  		const person = {
+	  			name: newName,
+	  			date: new Date().toISOString(),
+	  			important: Math.random() < 0.5,
+	  			id: newName + '-' + (persons.length + 1)
+			}
+			const contactExists = person => persons.some(p => p.name === person.name)
+			if (contactExists(person)) {
+			  		window.alert(`${person.name} is already added to phonebook`)
+			  		setNewName('')
+			} else {
+			  		setPersons(persons.concat(person))
+			  		setNewName('')
+			}
+		}
+	  	const handleNameChange = event => setNewName(event.target.value)
+	  	
+		return (
+			<div>
+		    	<h2>Phonebook</h2>
+		      	<form onSubmit={addContact}>
+		        	<div>
+		          		Write your name: <input value={newName} onChange={handleNameChange} />
+		        	</div>
+		        	<div>
+		          		<button id="add-contact-button" type="submit">Add Contact</button>
+		        	</div>
+		      	</form>
+		      	<h2>Contacts</h2>
+		  		<ul>
+			        {persons.map(person => 
+			          <Contacts key={person.id} person={person} />
+			        )}
+		    	</ul>
+			</div>
+		)
+	}
 
-  		if (contactExists(person)) {
-  			window.alert(`${person.name} is already added to phonebook`)
-  			setNewName('')
-  		} else {
-  			setPersons(persons.concat(person))
-  			setNewName('')
-  		}
-
-  	}
-  	const handleNameChange = event => setNewName(event.target.value)
-  	
-	return (
-		<div>
-	    	<h2>Phonebook</h2>
-	      	<form onSubmit={addContact}>
-	        	<div>
-	          		Write your name: <input value={newName} onChange={handleNameChange} />
-	        	</div>
-	        	<div>
-	          		<button id="add-contact-button" type="submit">Add Contact</button>
-	        	</div>
-	      	</form>
-	      	<h2>Contacts</h2>
-	  		<ul>
-		        {persons.map(person => 
-		          <Contacts key={person.id} person={person} />
-		        )}
-	    	</ul>
-		</div>
-	)
-}
-
-export default App
+	export default App
