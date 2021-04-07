fd11ab329987637bfd4eae9b3555dadc58abb2a0
Jetlum <jetlumajeti@gmail.com>
2021-04-07 23:50:41 +0200

    Update: Added an input field for filter

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [filter, setFilter] = useState('')
+
+  const handleFilterChange = event => setFilter(event.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <h2>Phonebook</h2>
+          <div>
+            Filter <input value={value} onChange={handleFilterChange} />
+          </div>
+      </div>
+    )
 }
 
 export default App;
