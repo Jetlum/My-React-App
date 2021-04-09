2408b1ec8f82f0904261e4a88addc0d75643f9a4
Jetlum <jetlumajeti@gmail.com>
2021-04-09 13:54:12 +0200

    Update: Filter countries by keyword

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Filter from './components/Filter'
 
-function App() {
+const App = () => {
 
+  const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
 
-  const handleFilterChange = event => setFilter(event.target.value)
+   useEffect(() => {
+        axios
+          .get('https://restcountries.eu/rest/v2/all')
+          .then(response => {
+          const countries = response.data
+          setCountries(countries)
+       })
+      }, [])
 
+  console.log('Countries outside: ', countries)
+  const handleFilterChange = event => setFilter(event.target.value)
+  console.log(filter)
+  const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()));
+  console.log('filter: ', filteredCountries)
+  
   return (
       <div>
-          <h2>Phonebook</h2>
+          <Filter value={filter} onChange={handleFilterChange} />
           <div>
-            Filter <input value={value} onChange={handleFilterChange} />
+            <ul>
+              {filteredCountries.map(countries => 
+                <li key={countries.id}>{countries.name} {countries.alpha2Code}</li>
+              )}
+            </ul>
           </div>
       </div>
-    )
+    ) 
 }
 
 export default App;
