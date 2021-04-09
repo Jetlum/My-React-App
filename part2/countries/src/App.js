d9a29968d6328a1223e38b92cdbea89a04f74389
Jetlum <jetlumajeti@gmail.com>
2021-04-09 15:38:57 +0200

    Update: Display basic country data when filter matches only one country

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -15,21 +15,32 @@ const App = () => {
           setCountries(countries)
        })
       }, [])
-
-  console.log('Countries outside: ', countries)
+  
   const handleFilterChange = event => setFilter(event.target.value)
-  console.log(filter)
   const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()));
   console.log('filter: ', filteredCountries)
-  
+  const singleCountry = filteredCountries.length === 1
+  console.log("1shi",filteredCountries[0])
+
   return (
       <div>
           <Filter value={filter} onChange={handleFilterChange} />
           <div>
             <ul>
-              {filteredCountries.map(countries => 
-                <li key={countries.id}>{countries.name} {countries.alpha2Code}</li>
-              )}
+              { singleCountry ?
+                <>
+                   <h1> {filteredCountries[0].name} </h1>
+                    <p> Capital: {filteredCountries[0].capital}</p>
+                    <p> Population: {filteredCountries[0].population}</p>
+                    <img src={filteredCountries[0].flag} width="200" height="200" />
+                </>
+                :
+                filteredCountries.map(countries => 
+                  <li key={countries.id}> 
+                    {countries.name} {countries.alpha2Code}
+                  </li>
+                )
+              }
             </ul>
           </div>
       </div>
