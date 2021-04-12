dafc5f20522968f1c3a01edfa8f010eecd3d71d0
Jetlum <jetlumajeti@gmail.com>
2021-04-12 22:32:16 +0200

    Update: Conditional scenarios for different filter inputs

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -20,7 +20,9 @@ const App = () => {
   const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()));
   console.log('filter: ', filteredCountries)
   const singleCountry = filteredCountries.length === 1
+  const multipleCountries = (filteredCountries.length > 10 && filteredCountries.length < 250)
   console.log("1shi",filteredCountries[0])
+  console.log("countries",countries)
 
   return (
       <div>
@@ -32,12 +34,24 @@ const App = () => {
                    <h1> {filteredCountries[0].name} </h1>
                     <p> Capital: {filteredCountries[0].capital}</p>
                     <p> Population: {filteredCountries[0].population}</p>
-                    <img src={filteredCountries[0].flag} width="200" height="200" />
-                </>
+                    <h1> Languages</h1>
+                      <ul>
+                        {filteredCountries[0].languages.map(lang => 
+                          <li key={lang.iso639_1}> 
+                            {lang.name}
+                          </li>
+                        )}
+                      </ul>
+                    <img src={filteredCountries[0].flag} alt='Country flag' width="200" height="200" />
+                </> :
+                multipleCountries ? 
+                  <>
+                    <p> Too many matches, specify another filter</p>
+                  </>
                 :
-                filteredCountries.map(countries => 
-                  <li key={countries.id}> 
-                    {countries.name} {countries.alpha2Code}
+                filteredCountries.map(country => 
+                  <li key={country.id}> 
+                    {country.name} {country.alpha2Code}
                   </li>
                 )
               }
