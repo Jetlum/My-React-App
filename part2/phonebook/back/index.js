a8d4543f9e04bd82c9e2169a349d9cbb645b588a
Jetlum <jetlumajeti@gmail.com>
2021-04-27 00:55:40 +0200

    Update: Implemented functionality to add new phonebook entries

--- part2/phonebook/back/index.js
+++ part2/phonebook/back/index.js
@@ -39,9 +39,12 @@ app.get('/info', (request, response) => {
 	response.send(`<p>Phonebook has info for ${contacts.length} people<br> ${new Date()}</p>`)
 })
 
+app.get('/api/contacts', (request, response) => {
+  response.json(contacts)
+})
+
 app.get('/api/contacts/:id', (request, response) => {
 	const id = request.params.id
-	console.log(id)
 	const contact = contacts.find(contact => contact.id === id)
 
 	if(contact) {
@@ -49,6 +52,40 @@ app.get('/api/contacts/:id', (request, response) => {
 	} else {
 		response.status(404).end()
 	}
+})
+
+const generateId = (name) => {
+  	const maxId = contacts.length > 0
+    ? name + '-' + (contacts.length + 1)
+    : 0
+  return maxId
+}
+
+app.post('/api/contacts', (request, response) => {
+	const body = request.body
+	if(!body.name && !body.number) {
+		return response.status(400).json({
+			error: 'Content is missing'
+		})
+	}
+	
+	const contact = {
+		name: body.name,
+		number: body.number,
+		date: new Date(),
+		important: body.important || false,
+		id: generateId(body.name),
+	}
+
+	contacts = contacts.concat(contact)
+
+	response.json(contact)
+})
+
+app.delete('/api/contacts/:id', (request, response) => {
+	const id = request.params.id
+	contacts = contacts.filter(contact => contact.id !== id)
+	response.status(204).end()
 })
 
 const PORT = 3001
