2ab30f56a515d1668530546a87f0a5559c2b1e76
Jetlum <jetlumajeti@gmail.com>
2021-04-27 01:11:36 +0200

    Update: Error handling for new and already existing entries

--- part2/phonebook/back/index.js
+++ part2/phonebook/back/index.js
@@ -4,6 +4,7 @@ const http = require('http')
 
 app.use(express.json())
 
+// Contacts(Resources)
 let contacts = [
 	{
 		"name": "lum",
@@ -30,19 +31,19 @@ let contacts = [
 	    
 	},
 ]
-
+// An event handler, that is used to handle HTTP GET requests made to the application's / root
 app.get('/', (request, response) => {
-	response.json(contacts)
+	response.send("Hello World!")
 })
-
+// /info writes some info
 app.get('/info', (request, response) => {
 	response.send(`<p>Phonebook has info for ${contacts.length} people<br> ${new Date()}</p>`)
 })
-
+// Get route fetches all resourses
 app.get('/api/contacts', (request, response) => {
   response.json(contacts)
 })
-
+// Get route using the id of contact fetches a single resourse
 app.get('/api/contacts/:id', (request, response) => {
 	const id = request.params.id
 	const contact = contacts.find(contact => contact.id === id)
@@ -53,22 +54,22 @@ app.get('/api/contacts/:id', (request, response) => {
 		response.status(404).end()
 	}
 })
-
+// generateId() generates a new id, taking the name parameter and adding contacts.length + 1
+// i.e. if contacts length is 3 will create: jetlum-4
 const generateId = (name) => {
   	const maxId = contacts.length > 0
     ? name + '-' + (contacts.length + 1)
     : 0
   return maxId
 }
-
+// Post route for adding new entries
 app.post('/api/contacts', (request, response) => {
 	const body = request.body
 	if(!body.name && !body.number) {
 		return response.status(400).json({
-			error: 'Content is missing'
+			error: 'Name and number are missing'
 		})
 	}
-	
 	const contact = {
 		name: body.name,
 		number: body.number,
@@ -76,18 +77,24 @@ app.post('/api/contacts', (request, response) => {
 		important: body.important || false,
 		id: generateId(body.name),
 	}
-
+	// Check if the name already exists
+	const contactExists = contact => contacts.some(c => c.name === contact.name)
+	// Add an error message if the name already exists
+	if(contactExists) {
+		return response.status(400).json({
+			error: 'Name must be unique'
+		})
+	}
 	contacts = contacts.concat(contact)
-
 	response.json(contact)
 })
-
+// Delete route for deleting resources
 app.delete('/api/contacts/:id', (request, response) => {
 	const id = request.params.id
 	contacts = contacts.filter(contact => contact.id !== id)
 	response.status(204).end()
 })
-
+// Listen to HTTP requests sent to the port 3001
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
