a868d72aa08f6cdb737a6d760aaaf24575c2bb44
Jetlum <jetlumajeti@gmail.com>
2021-05-04 01:44:08 +0200

    Clean Code: Beautify code

--- part2/phonebook/back/mongo.js
+++ part2/phonebook/back/mongo.js
@@ -6,11 +6,10 @@ if (process.argv.length < 3) {
 }
 
 const password	=	process.argv[2]
-const name 		=	process.argv[3]
+const name		=	process.argv[3]
 const number 	=	process.argv[4]
 
-const url =
-  `mongodb+srv://lumi_mongo:${password}@cluster0.qj9to.mongodb.net/phonebook?retryWrites=true&w=majority`
+const url = `mongodb+srv://lumi_mongo:${password}@cluster0.qj9to.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
