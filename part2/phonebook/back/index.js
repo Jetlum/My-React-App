be530a5b213e81728f09a8fcaa85ca00370ec5e9
Jetlum <jetlumajeti@gmail.com>
2021-05-08 01:44:41 +0200

    Update: Move error handling of the application to a new error handler miiddleware

--- part2/phonebook/back/index.js
+++ part2/phonebook/back/index.js
@@ -9,7 +9,7 @@ const Contact = require('./models/contacts')
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-
+app.use(bodyParser.json());
 const customLogger = morgan((tokens, req, res) => {
   const tinyLog = [
     tokens.method(req, res),
@@ -24,8 +24,12 @@ const customLogger = morgan((tokens, req, res) => {
   return req.method === 'POST' ? `${tinyLog} ${JSON.stringify(req.body)}` : tinyLog;
 });
 
-app.use(bodyParser.json());
+
 app.use(customLogger);
+
+const NAME_MISSING = { error: 'name missing' };
+const NUMBER_MISSING = { error: 'number missing' };
+const CONTACT_NOT_FOUND = { error: 'contact not found' };
 
 // An event handler that is used to handle HTTP GET requests made to the application's / root
 app.get('/', (request, response) => {
@@ -36,20 +40,22 @@ app.get('/info', (request, response) => {
 	response.send(`<p>Phonebook has info for ${contacts.length} people<br> ${new Date()}</p>`)
 })
 // Get route fetches all resourses
-app.get('/api/contacts', (request, response) => {
+app.get('/api/contacts', (request, response, next) => {
   Contact.find({}).then(contacts => {
     response.json(contacts)
   })
+  .catch(error => next(error));
 })
 // Get route using the id of contact fetches a single resourse
-app.get('/api/contacts/:id', (request, response) => {
+app.get('/api/contacts/:id', (request, response, next) => {
 	Contact.findById(request.params.id).then(contact => {
 		if (contact) {
       		response.json(contact.toJSON());
     	} else {
-      		response.status(404).end();
+      		next(CONTACT_NOT_FOUND);
     	}
 	})
+	.catch(error => next(error));
 })
 // generateId() generates a new id, taking the name parameter and adding contacts.length + 1
 // i.e. if contacts length is 3 will create: jetlum-4
@@ -61,30 +67,32 @@ app.get('/api/contacts/:id', (request, response) => {
 }*/
 
 // Post route for adding new entries
-app.post('/api/contacts', (request, response) => {
+app.post('/api/contacts', (request, response, next) => {
 	const body = request.body
-	if(!body.name && !body.number) {
-		return response.status(400).json({
-			error: 'Name and number are missing'
+	if(!body.name) {
+		next(NAME_MISSING);
+	} else if (!body.number) {
+		next(NUMBER_MISSING);
+	} else {
+		const contact = new Contact({
+			name: body.name,
+			number: body.number,
+			date: new Date(),
+			important: body.important || false,
 		})
+		// Check if the name already exists
+		//const contactExists = contact => contacts.some(c => c.name === contact.name)
+		// Add an error message if the name already exists
+		/*if(!contactExists) {
+			return response.status(400).json({
+				error: 'Name must be unique'
+			})
+		}*/
+		contact.save().then(savedContact => {
+	    	response.json(savedContact.toJSON())
+	  	})
+	  	.catch(error => next(error));
 	}
-	const contact = new Contact({
-		name: body.name,
-		number: body.number,
-		date: new Date(),
-		important: body.important || false,
-	})
-	// Check if the name already exists
-	//const contactExists = contact => contacts.some(c => c.name === contact.name)
-	// Add an error message if the name already exists
-	/*if(!contactExists) {
-		return response.status(400).json({
-			error: 'Name must be unique'
-		})
-	}*/
-	contact.save().then(savedContact => {
-    	response.json(savedContact.toJSON())
-  	})
 })
 // Delete route for deleting resources
 app.delete('/api/contacts/:id', (request, response) => {
@@ -92,7 +100,7 @@ app.delete('/api/contacts/:id', (request, response) => {
 		.then(result => {
 			response.status(204).end()
 		})
-		.catch(error => next(error))
+ 		.catch(error => next(error));
 })
 
 const unknownEndpoint = (request, response) => {
@@ -104,6 +112,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+	const { message, name, kind } = error;
+  	console.error(message);
+	if (name === 'CastError' && kind == 'ObjectId') {
+		return res.status(400).send({ error: 'malformatted id' });
+  	} else if (error === NAME_MISSING || error === NUMBER_MISSING) {
+    	return res.status(400).json(error);
+  	} else if (error === CONTACT_NOT_FOUND) {
+    	return res.status(404).end();
+  	} else next(error);
+};
+app.use(errorHandler);
 
 // Listen to HTTP requests sent to the port 3001
 const PORT = process.env.PORT || 3001
